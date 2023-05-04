94c8c2e4be0bba69c65bad47e8ff97b7aeff9678
italocirillo <italocirillo@gmail.com>
2023-05-04 23:12:11 +0200

    ESERCIZIO COMPLETO + BONUS

--- index.php
+++ index.php
@@ -40,6 +40,11 @@ $hotels = [
 
 ];
 
+if (isset($_GET['parcheggio']) && isset($_GET['voto'])) {
+    $parcheggio = $_GET['parcheggio'];
+    $voto = $_GET['voto'];
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -59,9 +64,29 @@ $hotels = [
 <body>
     <div class="container">
         <h1 class="text-center py-3">Tabella Hotel</h1>
-        <div class="text-center py-2">
+        <div class="text-center py-3">
             <img class="stelle" src="./img/stelle.png" alt="stelle">
         </div>
+        <!-- Form -->
+        <form class="py-2" action="index.php" method="GET">
+            <div class="d-flex gap-3">
+                <select class="form-select" name="parcheggio">
+                    <option value="2">Seleziona il parcheggio(tutti)</option>
+                    <option value="1">SI</option>
+                    <option value="0">NO</option>
+                </select>
+                <select class="form-select" name="voto">
+                    <option value="0">Seleziona il voto(tutti)</option>
+                    <option value="1">> 1</option>
+                    <option value="2">> 2</option>
+                    <option value="3">> 3</option>
+                    <option value="4">> 4</option>
+                    <option value="5">> 5</option>
+                </select>
+                <button type="submit" class="btn btn-success">INVIA</button>
+            </div>
+        </form>
+
         <table class="table table-dark table-striped table-hover">
             <thead>
                 <tr class="text-center">
@@ -74,27 +99,29 @@ $hotels = [
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($hotels as $key => $hotel) { ?>
-                    <tr class="text-center">
-                        <th scope="row"><?php echo $key + 1 ?></th>
-                        <td><?php echo $hotel['name'] ?></td>
-                        <td><?php echo $hotel['description'] ?></td>
-                        <td>
-                            <?php
-                            if ($hotel['parking'] === true) {
-                                echo "SI";
-                            } else {
-                                echo "NO";
-                            }
-                            ?>
-                        </td>
-                        <td><?php echo $hotel['vote'] ?></td>
-                        <td><?php echo $hotel['distance_to_center'] ?></td>
-                    </tr>
-                <?php } ?>
+                <?php
+                foreach ($hotels as $key => $hotel) {
+                    if (($parcheggio == 2 && $voto <= $hotel['vote']) || ($parcheggio == $hotel['parking'] && $voto == 0) || ($parcheggio == $hotel['parking'] && $voto <= $hotel['vote']) || ($parcheggio == 2 && $voto == 0)) { ?>
+                        <tr class="text-center">
+                            <th scope="row"><?php echo $key + 1 ?></th>
+                            <td><?php echo $hotel['name'] ?></td>
+                            <td><?php echo $hotel['description'] ?></td>
+                            <td>
+                                <?php
+                                if ($hotel['parking'] === true) {
+                                    echo "SI";
+                                } else {
+                                    echo "NO";
+                                }
+                                ?>
+                            </td>
+                            <td><?php echo $hotel['vote'] ?></td>
+                            <td><?php echo $hotel['distance_to_center'] ?></td>
+                        </tr>
+                <?php }
+                } ?>
             </tbody>
         </table>
-
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
