b16e84624486941f56ba8a35368f4b622dea08f5
italocirillo <italocirillo@gmail.com>
2023-05-04 18:44:52 +0200

    ciclo for each nell'array

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 
@@ -9,7 +53,12 @@
 </head>
 
 <body>
-
+    <div class="container">
+        <h1>Tabella Hotel</h1>
+        <?php foreach ($hotels as $hotel) { ?>
+            <h2><?php echo $hotel['name'] ?></h2>
+        <?php } ?>
+    </div>
 </body>
 
 </html>
